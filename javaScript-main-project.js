5854bafb70d52a9ca8a563cc1472f5a6f05ead92
Ernest5621 <ernestpopovic5621@gmail.com>
2024-11-02 16:02:18 +0200

    Upgrading switching in Expected-section

--- javaScript-main-project.js
+++ javaScript-main-project.js
@@ -8,32 +8,34 @@ document.addEventListener('DOMContentLoaded', () => {
     let isExtended = false;
     document.querySelector('.image-expectation').addEventListener('click', (event) => {
       event.preventDefault();
-      ImageList[0].style.transform = 'translateX(60%)';
-      ImageList[1].style.transform = 'translateX(-60%)';
+      setTimeout(()=> {
+          ImageList[0].style.transform = 'translateX(45%) scale(0.7)';
+          ImageList[1].style.transform = 'translateX(-45%) scale(0.7)';
+      },100);
       if (!isExtended) {
         ImageList[1].style.zIndex = '100';
         setTimeout(() => {
-          ImageList[0].style.zIndex = '1000';
-          ImageList[0].style.transform = 'translateX(0%) scale(1)';
-          ImageList[1].style.transform = 'translateX(0%) scale(0.6)';
-        }, 450);
+            ImageList[0].style.transform = 'translateX(0%) scale(1)';
+            ImageList[1].style.transform = 'translateX(0%) scale(0.6)';
+            ImageList[0].style.zIndex = '1000';
+        }, 500);
         isExtended = true;
-      } else {
+    } else {
         ImageList[0].style.zIndex = '100';
         setTimeout(() => {
-          ImageList[1].style.zIndex = '1000';
-          ImageList[0].style.transform = 'translateX(0%) scale(0.6)';
-          // ImageList[0].style.transform = 'translateX(0%)';
-          ImageList[1].style.transform = 'translateX(0%) scale(1)';
-        }, 450);
+            ImageList[1].style.zIndex = '1000';
+            ImageList[0].style.transform = 'translateX(0%) scale(0.6)';
+            // ImageList[0].style.transform = 'translateX(0%)';
+            ImageList[1].style.transform = 'translateX(0%) scale(1)';
+        }, 500);
         isExtended = false;
-      }
-    });
-  });
-  // to see de facto width of viewport
-  const infoWindow = document.createElement('div');
-  document.body.appendChild(infoWindow);
-  infoWindow.className = 'info-window';
+    }
+});
+});
+// to see de facto width of viewport
+const infoWindow = document.createElement('div');
+document.body.appendChild(infoWindow);
+infoWindow.className = 'info-window';
   window.addEventListener('resize', () => {
     const windowWidth = document.body.clientWidth;
     infoWindow.textContent = `${windowWidth}`;
